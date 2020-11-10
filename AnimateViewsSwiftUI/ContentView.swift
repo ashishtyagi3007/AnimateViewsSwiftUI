41eaabc9b91d50b49e1441df60b0dbca54c4a4b9
Ashish Tyagi <ashisht@dewsolutions.in>
2020-11-10 10:16:03 +0530

    Animate Views with beautiful animation

--- AnimateViewsSwiftUI/ContentView.swift
+++ AnimateViewsSwiftUI/ContentView.swift
@@ -7,10 +7,20 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var isRotated = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Rotate") {
+                self.isRotated.toggle()
+            }
+            Spacer()
+            ForEach(0..<6) { index in
+                RectangleView(isRotated: $isRotated, index: index)
+            }
+        }
     }
 }
 
